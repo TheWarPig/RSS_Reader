6b57f98cf12d0969993c5c86537e97fdc8783c09
Ido Strassberg <idostrassberg123@gmail.com>
2022-10-05 17:59:32 +0300

    Created RSS Reader that presents first 5 entries from the daily and shows summary and link

---
+++ main.py
@@ -0,0 +1,10 @@
+import feedparser
+
+feed = feedparser.parse('http://rss.art19.com/the-daily')
+entries = feed.entries
+print(feed.feed.title)
+print(len(entries))
+for i in range(5):
+    print(entries[i].title)
+    print(f"Summary: " + entries[i].summary)
+    print("Link: " + entries[i].link)
