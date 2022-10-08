fb9cf0d4791d44e0e21865139342c4a6b6be844c
Ido Strassberg <idostrassberg123@gmail.com>
2022-10-08 18:48:16 +0300

    Finally works in MAC OS!

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import feedparser
 import requests
 
-# is_https = "https://" in 'https://news.google.com/news?ned=us&output=rss'[:len("https://")]
-# if is_https:
-#   feed_content = requests.get('https://news.google.com/news?ned=us&output=rss')
-#   feed = feedparser.parse(feed_content.text)
-# elif not is_https:
-#   feed = feedparser.parse('https://news.google.com/news?ned=us&output=rss')
-
 feed_content = requests.get('http://rss.art19.com/the-daily')
 feed = feedparser.parse(feed_content.text)
 entries = feed.entries
